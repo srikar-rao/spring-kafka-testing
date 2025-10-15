69cd6dfbeae02c9c3348a71f3f876a6947bd5e7a
srikar-rao <67662872+srikar-rao@users.noreply.github.com>
2025-10-15 17:24:49 -0400

    master commit

---
+++ src/main/java/com/dev/kafka/KafkaIntegrationTestingApplication.java
@@ -0,0 +1,13 @@
+package com.dev.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaIntegrationTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaIntegrationTestingApplication.class, args);
+	}
+
+}
